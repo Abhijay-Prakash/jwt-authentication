f2456a2035e55ac00b3d3784f0c154ba7ffde957
Abhijay-Prakash <148245052+Abhijay-Prakash@users.noreply.github.com>
2025-01-29 23:05:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on PORT ${PORT}`);
 });
